c4800614b8cbbd25dd85e834fed5e9a25ad10594
GabrielMarrogn <gabrielosantos2012@hotmail.com>
2024-02-23 19:52:11 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        sc.nextLine();
+
+        for(int i = 0; i< a; i++){
+            sc.nextLine();
+        }
+
+        for(int i = 0; i < a; i++){
+            System.out.println("gzuz");
+        }
+
+        sc.close();
+    }
+}
